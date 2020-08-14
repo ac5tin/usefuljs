0d13d6c4a3a3c48966bab789f627f40cb86cee08
ac5tin <austinchang4@gmail.com>
2020-08-14 13:36:09 +0800

    added more functions to modern

--- modern/index.ts
+++ modern/index.ts
@@ -1,22 +1,20 @@
 import { ajaxReq } from "./interface.ts";
 
 class useful {
+    /** array dedupe 
+     * @param {Array} x an array
+     * @returns {Array}
+    */
     static arr_dedupe = <T>(x:Array<T>):Array<T>=>{
         return x.filter((item,index)=> x.indexOf(item) === index);
     }
 
+    /** wrapper around the Fetch API */
     static ajax = async<T>({method="GET" , url="",data=undefined,signal=undefined,headers={},encodeuri=true, cors=undefined , formdata=undefined, fetcher=undefined, jsonbody=true, jsonheader=true , json=true}:ajaxReq):Promise<T>=>{
         try{
             if(fetcher === undefined)fetcher = fetch;
             method = method.toUpperCase();
-            type requestBody = {
-                method:string,
-                body?:any,
-                headers?:any,
-                mode?:string,
-                signal?:AbortSignal
-            }
-            const reqBody:requestBody = {
+            const reqBody:RequestInit = {
                 method:method.toUpperCase(),
             };
 
@@ -66,6 +64,76 @@ class useful {
             if(json)content = await res.json();
             return content;
         }catch(err){throw err}
+    }
+
+    /** last item of an array */
+    static arr_last = <T>(arr:Array<T>):T=> arr[arr.length - 1];
+
+    /** split array into chunks of smaller arrays */
+    static arr_chunk = <T>(arr:Array<T>,length:number):Array<T>[]=>{
+        const retMe:Array<T>[] = [];
+        for(let i = 0;i < arr.length;i+=length){
+            retMe.push(arr.slice(i,i+length));
+        }
+        return retMe;
+    }
+
+    /** generates uuidv4 string */
+    static uuidv4 = ():string=> "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c=> {
+        const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    
+      /** generate a short uid */
+    static shortuid = ():string=>{
+        let firstPart = (Math.random() * 46656) | 0;
+        let secondPart = (Math.random() * 46656) | 0;
+        const fp = ("000" + firstPart.toString(36)).slice(-3);
+        const sp = ("000" + secondPart.toString(36)).slice(-3);
+        return fp + sp;
+    }
+
+    /** pause */
+    static sleep = (milliseconds:number)=> new Promise(resolve => setTimeout(resolve,milliseconds));
+
+    /** filter out empty NaN values from array */
+    static f_arr = <T>(arr:Array<T>):Array<T>=>{
+        let index = -1;
+        const arr_length = arr.length;
+        let resIndex = -1;
+        const results:Array<T> = [];
+        while(++index < arr_length){
+            const value:T = arr[index];
+            if(value){
+                results[++resIndex] = value;
+            }
+        }
+        return results;
+    }
+
+    /** string to hex code */
+    static hexCode = (x:string):string =>{
+        let hex;
+
+        let result = "";
+        for (let i=0; i<x.length; i++) {
+            hex = x.charCodeAt(i).toString(16);
+            result += ("000"+hex).slice(-4);
+        }
+
+        return result
+    }
+
+    /** array of json to csv */
+    static toCSV = (json:any[]):string =>{
+        const jv:any[] = Object.values(json);
+        let csv = "";
+        let keys = (jv[0] && Object.keys(jv[0])) || [];
+        csv += keys.join(",") + "\n";
+        for(let line of jv){
+            csv += keys.map(key => line[key]).join(",") + "\n";
+        }
+        return csv;
     }
     
 }
